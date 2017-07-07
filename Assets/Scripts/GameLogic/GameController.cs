55d0f86f38c262e74b6b3d66266ac4ab9d9b35e0
kogelnig <kogelnig_philipp@gmx.at>
2017-07-07 13:55:11 +0200

    add some sound logic

---
+++ Assets/Scripts/GameLogic/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField] private GameObject[] spawnableUIPrefabs;
+    private List<GameObject> currentlySpawnablePrefabs = new List<GameObject>();
+    private int currentLevel = 1;
+
+    private void StartCurrentLevel() {
+        InitSpawnableUIsForLevel();
+        BroadcastMessage("PlaySound", "MorningCrow", SendMessageOptions.DontRequireReceiver);
+    }
+
+    private void InitSpawnableUIsForLevel() {
+        currentlySpawnablePrefabs.Clear();
+        foreach (var spawnableUIPrefab in spawnableUIPrefabs)
+        {
+            if (spawnableUIPrefab.GetComponent<LevelRequirement>().IsValidForLevel(currentLevel))
+            {
+                currentlySpawnablePrefabs.Add(spawnableUIPrefab);
+            }
+        }
+    }
+    
+	void Start () {
+        StartCurrentLevel();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+    [SerializeField] private PlaySound[] sounds;
+
+	private void PlaySound(string key) {
+		foreach (var sound in sounds)
+        {
+            if (key == sound.key) {
+                Play(sound.clip);
+                return;
+            }
+        }
+	}
+
+    private void Play(AudioClip clip) {
+        GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+}
