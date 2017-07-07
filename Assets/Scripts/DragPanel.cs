e90ec9247aed2059cde9feee46b19404ff950cd8
Guilherme Rodrigues <guilhermecsrodrigues@gmail.com>
2017-07-07 12:24:21 +0200

    Fixed the draggable script

--- Assets/Scripts/DragPanel.cs
+++ Assets/Scripts/DragPanel.cs
@@ -3,49 +3,19 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class DragPanel : MonoBehaviour, IPointerDownHandler, IDragHandler {
+public class DragPanel : MonoBehaviour, IBeginDragHandler, IDragHandler {
 
 	private Vector2 pointerOffset;
-	private RectTransform canvasRectTransform;
-	private RectTransform panelRectTransform;
 
-	void Awake () {
-		Canvas canvas = GetComponentInParent <Canvas>();
-		if (canvas != null) {
-			canvasRectTransform = canvas.transform as RectTransform;
-			panelRectTransform = transform.parent as RectTransform;
-		}
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData data)
+	{
+		var position = new Vector2 (transform.position.x, transform.position.y);
+		pointerOffset =  data.position - position;
 	}
-
-	public void OnPointerDown (PointerEventData data) {
-		panelRectTransform.SetAsLastSibling ();
-		RectTransformUtility.ScreenPointToLocalPointInRectangle (panelRectTransform, data.position, data.pressEventCamera, out pointerOffset);
-	}
+	#endregion
 
 	public void OnDrag (PointerEventData data) {
-		if (panelRectTransform == null)
-			return;
-
-		Vector2 pointerPostion = ClampToWindow (data);
-
-		Vector2 localPointerPosition;
-		if (RectTransformUtility.ScreenPointToLocalPointInRectangle (
-			canvasRectTransform, pointerPostion, data.pressEventCamera, out localPointerPosition
-		)) {
-			panelRectTransform.localPosition = localPointerPosition - pointerOffset;
-		}
-	}
-
-	Vector2 ClampToWindow (PointerEventData data) {
-		Vector2 rawPointerPosition = data.position;
-
-		Vector3[] canvasCorners = new Vector3[4];
-		canvasRectTransform.GetWorldCorners (canvasCorners);
-
-		float clampedX = Mathf.Clamp (rawPointerPosition.x, canvasCorners[0].x, canvasCorners[2].x);
-		float clampedY = Mathf.Clamp (rawPointerPosition.y, canvasCorners[0].y, canvasCorners[2].y);
-
-		Vector2 newPointerPosition = new Vector2 (clampedX, clampedY);
-		return newPointerPosition;
+		transform.position = data.position - pointerOffset;// ClampToWindow(data);
 	}
 }
