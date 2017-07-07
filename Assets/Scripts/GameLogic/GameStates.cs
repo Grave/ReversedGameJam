2ef6d6dee49290995e1551893c96b182dfa1c325
kogelnig <kogelnig_philipp@gmx.at>
2017-07-07 16:14:38 +0200

    add default briefing

---
+++ Assets/Scripts/GameLogic/GameStates.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates {
+    INIT_GAME,
+    BRIEFING,
+    WORKING,
+    DAY_END,
+    GAME_END
+}
