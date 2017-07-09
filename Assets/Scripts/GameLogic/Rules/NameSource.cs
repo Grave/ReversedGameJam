22618d6bb698a3bc8ea55d0213edc8e95c2dc500
Guilherme Rodrigues <guilhermecsrodrigues@gmail.com>
2017-07-09 11:04:33 +0200

    Added different fluff text options

--- Assets/Scripts/GameLogic/Rules/NameSource.cs
+++ Assets/Scripts/GameLogic/Rules/NameSource.cs
@@ -4,14 +4,19 @@ using UnityEngine.UI;
 public class NameSource : MonoBehaviour 
 {
 	[SerializeField] private Text textField;
-	[SerializeField] private string fluffTextAfterName;
+	[SerializeField] private string[] fluffTextAfterName;
 
 	private string nameSource;
+
+	private string GetRandomFluffText()
+	{
+		return fluffTextAfterName [Random.Range (0, fluffTextAfterName.Length)];
+	}
 
 	public void SetNameSource(string newName)
 	{
 		nameSource = newName;
-		textField.text = newName + " " + fluffTextAfterName;
+		textField.text = newName + " " + GetRandomFluffText();
 	}
 
 	public string GetNameSource()
